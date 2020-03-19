7e7ce9818946c2f28b35501f53921883e67c4280
Ketsiree <funny24073011@gmail.com>
2020-03-19 11:04:00 +0700

    ffff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './Task.css';
+import { firestore } from 'firebase';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [task,setTask] = useState([
+    { id: 1,name: 'do homework'},
+    { id: 2,name: 'write node js'}
+  ])
+  
+useEffect (() => {})
+
+const retriveData = () => {
+  firestore.Collection("tasks").onSnapshotS ( (snapshot)) =>{
+console.log(snapshot.docs)
+let myTask = snapshot.docs.map{ d => {
+  const {id,name} = d.data()
+  console.log (id,name)
+  return {id,name}
+}}
+  setTask
+  })
+}
+
+const deleteTask = (id) => {
+  firestore.collection("task").docs(id+'').delete()
+}
+
+
+const renderTask = () => {
+  console.log(task)
+  if (tasks && tasks.length) {
+    return tasks.map((task,index) =>{
+      return (
+        <li key = {index} >
+          {task.id} : {task.name}
+
+          
+          <button onClick = { () => deleteTask{task.id}}> delete </button>
+          <button onClick = { () => deleteTask{task.id}}> delete </button>
+
+          
+        </li>
+      )
+    })
+  } else {
+    return <li> No task </li>
+  }
 }
 
 export default App;

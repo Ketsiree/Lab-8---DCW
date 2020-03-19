8bcae6e8785f826e411db54032d7efe27ef6e3d5
Ketsiree <funny24073011@gmail.com>
2020-03-19 11:14:44 +0700

    ่่่่่่่้fffff

--- src/config.js
+++ src/config.js
@@ -1,1 +1,23 @@
-const config = 
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCNssSG1hOFstrFtD4kk4IvrlkNmTaqycc",
+    authDomain: "lab-8-dcw.firebaseapp.com",
+    databaseURL: "https://lab-8-dcw.firebaseio.com",
+    projectId: "lab-8-dcw",
+    storageBucket: "lab-8-dcw.appspot.com",
+    messagingSenderId: "905798570341",
+    appId: "1:905798570341:web:ef15bbb6bf5078e32fd18c",
+    measurementId: "G-4BTKCXHCZG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
